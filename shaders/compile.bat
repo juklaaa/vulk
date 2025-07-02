e4323f441f2752986fd241363651298e7c230c69
juka <juliach2137@gmail.com>
2025-07-02 08:46:31 +0200

    offscreen shaders

--- shaders/compile.bat
+++ shaders/compile.bat
@@ -1,3 +1,7 @@
 glslc.exe shader.vert -o nmap_v.spv
 glslc.exe shader.frag -o nmap_f.spv
+
+glslc.exe offscreenshader.vert -o offscreen_v.spv
+glslc.exe offscreenshader.frag -o offscreen_f.spv
+
 pause
